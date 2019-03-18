173e5eab927dbbd0f77fc2fadf1d3d96e00096f1
Estela Balboa <estela.balboa@gmail.com>
2019-03-18 20:03:40 +0100

    feat: generado el randomForestRegressor y entrenamiento con todos los datos

--- rfr_admission_prediction.py
+++ rfr_admission_prediction.py
@@ -1,7 +1,11 @@
 # Importar librerias necesarias
+from typing import List, Any, Tuple
+
 import numpy as np
 import pandas as pd
 from time import time
+
+import seaborn as sns
 from IPython.display import display
 
 # Importar aquí librerias de sklearn y fastai que se consideren necesarias
@@ -10,13 +14,16 @@ import os
 
 from pandas import DataFrame
 from scipy.constants import alpha
+from sklearn.ensemble import RandomForestRegressor
+
+from fastai.fastai import metrics
 
 os.chdir("/Users/ebalboa/Documents/AI-SATURDAYS/20190316/rfr-university-admission-predictor/")
 # Crear dataframe a partir de .csv
 input_data = pd.read_csv("data/admission.csv", sep=",")
 
 # Mostrar número de filas y columnas del dataframe
-display(input_data.info())
+display(input_data.shape)
 
 # Mostrar las primeras 10 filas
 display(input_data.head(10))
@@ -50,40 +57,84 @@ print("Nulos por columnas: " + '\n' + str(renamed_df.isnull().any()))
 # Mostrar información general del dataframe
 display(renamed_df.info())
 
-
 # Descripción analítica básica del dataframe
 display(renamed_df.describe())
 
 # Mostrar matriz de correlación de variables
 # Pista: explore plt.matshow y corr() de un dataframe
-matriz_corr: DataFrame = renamed_df.corr()
+corr_matrix = renamed_df.corr()
+# plot the heatmap
+sns.heatmap(corr_matrix)
+    #(corr_matrix, cmap='BuGn')
 
-display(plt.matshow(matriz_corr), interpolation='nearest')
+
+#display(plt.matshow(corr_matrix), interpolation='nearest')
 
 
+ax1 = plt.figure().add_subplot(111)
+plt.figure().colorbar(ax1.imshow(corr_matrix, interpolation="nearest"), ticks=[.4, 0.5, .6, .75, .8, .85, .90, .95, 1])
+plt.title('Correlation Matrix')
+labels = ['GRE', 'TOEFL', 'URat', 'SOP', 'LOR', 'CGPA', 'Research', 'Chance']
 
-fig = plt.figure()
-ax = fig.add_subplot(111)
-data = np.random.random((4,4))
-cax = ax.matshow(data, interpolation='nearest')
-fig.colorbar(cax)
+ax1.set_xticklabels(labels, fontsize=8)
+ax1.set_yticklabels(labels, fontsize=8)
 plt.show()
+
+sns.heatmap(corr_matrix,
+        xticklabels=corr_matrix.columns,
+        yticklabels=corr_matrix.columns)
 
 # Mostrar correlaciones como una función discreta entre las diferentes variables con una matriz
 # útil para apreciar relaciones lineales
+pd.plotting.scatter_matrix(renamed_df, diagonal='kde')
+plt.show()
 
 # Pista: explore pd.plotting.scatter_matrix
 
 # Crear un dataframe solo con la columna de la variable dependiente
 
-# Crear un dataframe con las variables independientes
+dep_df = renamed_df.Chance
 
+# Crear un dataframe con las variables independientes
+indep_df = renamed_df.drop('Chance', axis=1)
+display(indep_df.head(5))
 
 # Definir un RF con diferentes hiperparámetros (¡experimentar!)
-
+modelRfC: RandomForestRegressor = RandomForestRegressor(criterion="mse", max_depth=10,random_state=42,n_estimators=100)
 
 # Entrenar un RF con la totalidad del dataset
+trained_data: object = modelRfC.fit(indep_df, dep_df)
 
+trained_data
 
 ###### Snippet para imprimir resultados, df es la variable que refiere
 ###### al dataframe y clf al regresor, cambiarlas si es necesario
+
+
+feature_list: List[Tuple[Any, Any]] = list(zip(renamed_df.columns.values, modelRfC.feature_importances_))
+sorted_by_importance = sorted(feature_list, key=lambda x: x[1], reverse=True)
+
+for feat, value in sorted_by_importance:
+    print(feat, value)
+
+# Partir el test en cierta proporción (¡experimentar!)
+# X_train, X_test, y_train, y_test = # ?
+
+###### Snippet para imprimir resultados, X_train es la variable que refiere
+###### a la porcion de entrenamiento y X_test a la de test
+
+# print("El dataset de training tiene {} elementos.".format(X_train.shape[0]))
+# print("El dataset de testing tiene {} elementos.".format(X_test.shape[0]))
+
+# Definir un rfregressor
+
+# Entrenar el regresor con el dataset de train
+
+
+# Predecir valores para las variables independientes de test
+
+
+# Calcular la precisión
+# Pista: explorar sklearn.metrics
+# print("Precisión global: " + str(metrics.r2_score(y_test, predicted)))
+# print("MSE: " + str(metrics.mean_squared_error(y_test, predicted)))
